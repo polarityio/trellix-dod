55f2b33b4a170cd3bee58c642906f9cecd5b5937
Ed <ed@polarity.io>
2020-07-06 10:43:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Polarity FireEye Detection on Demand (DoD) Integration
+
+The Polarity FireEye Detection on Demand (DoD) integration allows Polarity to quickly identify malicious files contained within the FireEye platform.  The FireEye DoD service only supports looking up MD5 hashes.
+
+<img src="./assets/overlay.png" width="50%">  
+
+> The FireEye DoD endpoint is limited to 200 hash lookups per minute.
+
+For more information about FireEye's Detection on Demand capability please see: https://www.fireeye.com/solutions/detection-on-demand.html
+
+For information on the FireEye DoD API please see: https://fireeye.dev/apis/
+## Installation Instructions
+
+Installation instructions for integrations are provided on the [PolarityIO GitHub Page](https://polarityio.github.io/).
+
+## Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see:
+
+https://polarity.io/
---
+++ integration.js
@@ -0,0 +1,173 @@
+'use strict';
+
+const request = require('request');
+const config = require('./config/config');
+const async = require('async');
+const fs = require('fs');
+
+let Logger;
+let requestWithDefaults;
+
+const MAX_PARALLEL_LOOKUPS = 10;
+
+function startup(logger) {
+  let defaults = {};
+  Logger = logger;
+
+  const { cert, key, passphrase, ca, proxy, rejectUnauthorized } = config.request;
+
+  if (typeof cert === 'string' && cert.length > 0) {
+    defaults.cert = fs.readFileSync(cert);
+  }
+
+  if (typeof key === 'string' && key.length > 0) {
+    defaults.key = fs.readFileSync(key);
+  }
+
+  if (typeof passphrase === 'string' && passphrase.length > 0) {
+    defaults.passphrase = passphrase;
+  }
+
+  if (typeof ca === 'string' && ca.length > 0) {
+    defaults.ca = fs.readFileSync(ca);
+  }
+
+  if (typeof proxy === 'string' && proxy.length > 0) {
+    defaults.proxy = proxy;
+  }
+
+  if (typeof rejectUnauthorized === 'boolean') {
+    defaults.rejectUnauthorized = rejectUnauthorized;
+  }
+
+  requestWithDefaults = request.defaults(defaults);
+}
+
+function doLookup(entities, options, cb) {
+  let lookupResults = [];
+  let tasks = [];
+
+  Logger.debug(entities);
+  entities.forEach((entity) => {
+    let entityValue = entity.value.toLowerCase();
+    let requestOptions = {
+      method: 'GET',
+      uri: `https://internal-feapi.marketplace.apps.fireeye.com/hashes/${entityValue}`,
+      headers: {
+        'feye-auth-key': options.apiKey
+      },
+      json: true
+    };
+
+    Logger.trace({ requestOptions }, 'Request Options');
+
+    tasks.push(function (done) {
+      requestWithDefaults(requestOptions, function (error, res, body) {
+        Logger.trace({ body, statusCode: res ? res.statusCode : 'NA' });
+        let processedResult = handleRestError(error, entity, res, body);
+
+        if (processedResult.error) {
+          done(processedResult);
+          return;
+        }
+
+        done(null, processedResult);
+      });
+    });
+  });
+
+  async.parallelLimit(tasks, MAX_PARALLEL_LOOKUPS, (err, results) => {
+    if (err) {
+      Logger.error({ err: err }, 'Error');
+      cb(err);
+      return;
+    }
+
+    results.forEach((result) => {
+      if (result.body === null) {
+        lookupResults.push({
+          entity: result.entity,
+          data: null
+        });
+      } else {
+        lookupResults.push({
+          entity: result.entity,
+          data: {
+            summary: [`Is Malicious? ${result.body.is_malicious}`],
+            details: result.body
+          }
+        });
+      }
+    });
+
+    Logger.debug({ lookupResults }, 'Results');
+    cb(null, lookupResults);
+  });
+}
+
+function handleRestError(error, entity, res, body) {
+  let result;
+
+  if (error) {
+    return {
+      error: error,
+      detail: 'HTTP Request Error'
+    };
+  }
+
+  if (res.statusCode === 200 && body) {
+    // we got data!
+    result = {
+      entity: entity,
+      body: body
+    };
+  } else if (res.statusCode === 400) {
+    result = {
+      error: 'Bad Request',
+      detail: body.query_status
+    };
+  } else if (res.statusCode === 401) {
+    result = {
+      error: 'Unauthorized',
+      detail: body.query_status
+    };
+  } else if (res.statusCode === 403) {
+    result = {
+      error: 'Forbidden',
+      detail: body.query_status
+    };
+  } else if (res.statusCode === 429) {
+    result = {
+      error: 'Too Many Requests',
+      detail: body.query_status
+    };
+  } else {
+    result = {
+      error: body,
+      statusCode: res ? res.statusCode : 'Unknown',
+      detail: 'An unexpected error occurred'
+    };
+  }
+
+  return result;
+}
+
+function validateOptions(userOptions, cb) {
+  let errors = [];
+  if (
+    typeof userOptions.apiKey.value !== 'string' ||
+    (typeof userOptions.apiKey.value === 'string' && userOptions.apiKey.value.length === 0)
+  ) {
+    errors.push({
+      key: 'apiKey',
+      message: 'You must provide a valid API key'
+    });
+  }
+  cb(null, errors);
+}
+
+module.exports = {
+  doLookup: doLookup,
+  validateOptions: validateOptions,
+  startup: startup
+};
